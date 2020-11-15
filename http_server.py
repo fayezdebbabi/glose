0d249a914bee679007cc8ab3f57ffa3df60b155d
= <fayezdebbabi@gmail.com>
2020-11-15 23:11:35 +0100

    Added support for GET request with some manuel templating for html tags

--- http_server.py
+++ http_server.py
@@ -1,5 +1,5 @@
 import socket
-from urllib.parse import urlparse
+from pathlib import Path
 
 HOST = '127.0.0.1'
 PORT = 8080
@@ -16,10 +16,44 @@ def parse_http_request(data):
 
 
 def generate_http_response(method, path, protocol, headers, body):
-    response = "temporary response"
-    parsed_path = urlparse(path)
-    tmp_response = f"HTTP/1.1 200 OK\r\nContent-Length: {len(response)}\r\nContent-Type: text/html\r\n\r\n{response}\r\n"
-    return tmp_response
+    if (method == "GET"):
+        requested_file = Path("." + path)
+        if requested_file.exists():
+            if requested_file.is_file():
+                response_body_content = requested_file.read_text()
+                generated_response = f"HTTP/1.1 200 OK\r\nContent-Length: {len(response_body_content)}\r\nContent-Type: text/html\r\n\r\n{response_body_content}\r\n"
+                return generated_response
+            if requested_file.is_dir():
+                directory_result_list = ""
+                for entry in requested_file.iterdir():
+                    if entry.is_dir():
+                        tmp_file_reference = str(entry).split("/")[-1]
+                        if path == "/":
+                            directory_result_list = directory_result_list + \
+                                f"""<a href="{path + str(entry)}">{str(entry)}</a><br>"""
+                        else:
+                            directory_result_list = directory_result_list + \
+                                f"""<a href="{path + "/" + tmp_file_reference}">{str(tmp_file_reference)}</a><br>"""
+                for entry in requested_file.iterdir():
+                    if entry.is_file():
+                        tmp_file_reference = str(entry).split("/")[-1]
+                        if path == "/":
+                            directory_result_list = directory_result_list + \
+                                f"""<a href="{path + str(entry)}">{str(entry)}</a><br>"""
+                        else:
+                            directory_result_list = directory_result_list + \
+                                f"""<a href="{path + "/" + tmp_file_reference}">{str(tmp_file_reference)}</a><br>"""
+                response_body_content = directory_result_list
+                generated_response = f"HTTP/1.1 200 OK\r\nContent-Length: {len(response_body_content)}\r\nContent-Type: text/html\r\n\r\n{response_body_content}\r\n"
+                return generated_response
+        else:
+            response_body_content = "Resource not found"
+            generated_response = f"HTTP/1.1 404 Bad Request\r\nContent-Length: {len(response_body_content)}\r\nContent-Type: text/html\r\n\r\n{response_body_content}\r\n"
+            return generated_response
+    else:
+        response_body_content = "Method not supported"
+        generated_response = f"HTTP/1.1 400 Bad Request\r\nContent-Length: {len(response_body_content)}\r\nContent-Type: text/html\r\n\r\n{response_body_content}\r\n"
+        return generated_response
 
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
